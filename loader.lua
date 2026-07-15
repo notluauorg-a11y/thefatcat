833437171d1554b1960e98fe081a31753f058daf
igvro838 <notluauorg@gmail.com>
2026-07-16 05:17:38 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+repeat task.wait() until game:IsLoaded()
+if not (cloneref and hookmetamethod and clonefunction and iscclosure and islclosure and fireproximityprompt and firetouchinterest and getgc and hookfunction) then
+ game.Players.LocalPlayer:Kick('Ass executor, get a better executor')
+ return
+end
+--setclipboard(game.PlaceId)
+--print(game.PlaceId)
+if (game.PlaceId == 71832465156084 or game.PlaceId == 14386691987 or game.PlaceId == 17122385635) then
+ -- Basketball Legends
+ loadstring(game:HttpGet('https://api.jnkie.com/api/v1/luascripts/public/e658bc03d8ba8a84c882f69252868161fa8a4503e62d23f505a28adcaeb096b1/download'))()
+ return
+else
+ game.Players.LocalPlayer:Kick('Unsupported game')
+ return
+end
